ac19c9cc29dd49d86917bb4ce38b638931626814
sergey <bugakov.sa.88@gmail.com>
2018-02-27 20:50:06 +0300

    concatenated words

--- src/main/java/ConcatenatedWords.java
+++ src/main/java/ConcatenatedWords.java
@@ -1,42 +1,20 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ConcatenatedWords {
 
-    private Map<String, Boolean> cache1 = new HashMap<>();
-    private Map<String, Boolean> cache2 = new HashMap<>();
-
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        Arrays.sort(words, Comparator.comparingInt(String::length));
-        List<String> res = new ArrayList<>();
-        for (String word : words) {
-            if(!cache2.containsKey(word)){
-                cache2.put(word, isConcatenated(word, words, true));
-            }
-            if (cache2.get(word)) {
-                res.add(word);
-            }
-        }
-        return res;
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        return set.stream().filter(word -> isConcatenated(word, set)).collect(Collectors.toList());
     }
 
-    public boolean isConcatenated(String s, String[] words, boolean isFirstString) {
-        for (String word : words) {
-            if(word.length() > s.length()) {
-                return false;
-            }
-            if ((isFirstString && !word.equals(s)) || !isFirstString) {
-                if (0 < word.length() && s.startsWith(word)) {
-                    if (word.length() < s.length()) {
-                        String substring = s.substring(word.length());
-                        if(!cache1.containsKey(substring)){
-                            cache1.put(substring, isConcatenated(substring, words, false));
-                        }
-                        if (cache1.get(substring)) {
-                            return true;
-                        }
-                    } else {
-                        return true;
-                    }
+    private boolean isConcatenated(String s, Set<String> words) {
+        for (int i = 1; i < s.length(); i++) {
+            String head = s.substring(0, i);
+            if (words.contains(head)) {
+                String tail = s.substring(i);
+                if (words.contains(tail) || isConcatenated(tail, words)) {
+                    return true;
                 }
             }
         }
