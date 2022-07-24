88915281deef01d1e61beef6f6aa95eca5e996e8
bugakov-sa <bugakov.sa.88@gmail.com>
2022-07-24 11:39:14 +0300

    autoformat

--- src/main/java/CombinationSum.java
+++ src/main/java/CombinationSum.java
@@ -7,21 +7,20 @@ public class CombinationSum {
 
     private List<List<Integer>> combinationSum(int[] candidates, int target, int start) {
         List<List<Integer>> res = new ArrayList<>();
-        if(start == candidates.length || target < candidates[start]) {
+        if (start == candidates.length || target < candidates[start]) {
             return res;
         }
         int n = target / candidates[start];
-        for(int i = 0; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             int subTarget = target - candidates[start] * i;
             List<Integer> iCopies = new ArrayList<>(Collections.nCopies(i, candidates[start]));
-            if(subTarget > 0) {
+            if (subTarget > 0) {
                 List<List<Integer>> subLists = combinationSum(candidates, subTarget, start + 1);
-                for(List<Integer> subList : subLists) {
+                for (List<Integer> subList : subLists) {
                     subList.addAll(iCopies);
                 }
                 res.addAll(subLists);
-            }
-            else {
+            } else {
                 res.add(iCopies);
             }
         }
